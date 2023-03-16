24178d1f556bb5cb5066736b67dead3b1b88984a
Patrickc <darkxwarrior13@gmail.com>
2023-03-16 11:08:02 -0700

    Initial Commit - UltrasonicSensor

---
+++ UltrasonicSensor-BlindSpotMonitor/UltrasonicSensor-BlindSpotMonitor.ino
@@ -0,0 +1,14 @@
+//Programmer: Patrick Condon
+//Date: 3.16.2023
+//Program: Blind Spot Monitor
+
+/*This Program will determine distances to approaching vehicles that are in your blind spot*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
